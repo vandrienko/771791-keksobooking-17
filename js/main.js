bc5a6f23cf68969989c7270d87a15e86cf037bec
Vladimir <vandrienko1991@gmail.com>
2019-06-12 11:05:29 +0300

    Выполнил задание 2 - у блока .map уберите класс .map--faded.

---
+++ js/main.js
@@ -0,0 +1,16 @@
+
+
+
+
+
+
+
+
+
+
+
+// Задание №2 У блока .map уберите класс .map--faded.
+// Получаем элементы с классом map. Используем селектор.
+var map = document.querySelector('.map');
+// Удаляем класс map--faded у элемента map
+map.classList.remove('map--faded');
