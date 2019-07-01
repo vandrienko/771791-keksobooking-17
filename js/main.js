c144543dbfce53fba5fde28d3573ec4603b8588d
Vladimir <vandrienko1991@gmail.com>
2019-07-01 20:14:31 +0300

    Пины шаг 1

--- js/main.js
+++ js/main.js
@@ -140,7 +140,7 @@ var onButtonClick = function () {
 };
 
 // Отслеживаем нажатия на главный пин и вызываем функцию onButtonClick.
-pinMain.addEventListener('click', onButtonClick);
+// pinMain.addEventListener('click', onButtonClick);
 
 // module4-task2
 // 3.3. Поле «Тип жилья» влияет на минимальное значение поля «Цена за ночь»:
@@ -194,3 +194,52 @@ function onTimeinСhange() {
 function onTimeoutСhange() {
   timeinElement.value = timeoutElement.value;
 }
+
+// module5-task1
+var MIN_X = 0;
+var MAX_X = 1200;
+var MIN_Y = 130;
+var MAX_Y = 630;
+
+var mapPin = document.querySelector('.map__pin');
+// pinMain.addEventListener('click', onButtonClick);
+
+  pinMain.addEventListener('mousedown', function (evt) {
+
+  // Вызываю функцию от рисовки остальных пинов и разблокировки формы.
+  onButtonClick();
+
+  evt.preventDefault();
+  var startCoords = {
+    x: evt.clientX,
+    y: evt.clientY
+  };
+
+  var onMouseMove = function (moveEvt) {
+    moveEvt.preventDefault();
+
+    var shift = {
+      x: startCoords.x - moveEvt.clientX,
+      y: startCoords.y - moveEvt.clientY
+    };
+
+    startCoords = {
+      x: moveEvt.clientX,
+      y: moveEvt.clientY
+    };
+    console.log(startCoords);
+    mapPin.style.top = (mapPin.offsetTop - shift.y) + 'px';
+    mapPin.style.left = (mapPin.offsetLeft - shift.x) + 'px';
+  };
+
+  var onMouseUp = function (upEvt) {
+    upEvt.preventDefault();
+
+    document.removeEventListener('mousemove', onMouseMove);
+    document.removeEventListener('mouseup', onMouseUp);
+  };
+
+  document.addEventListener('mousemove', onMouseMove);
+  document.addEventListener('mouseup', onMouseUp);
+});
+
